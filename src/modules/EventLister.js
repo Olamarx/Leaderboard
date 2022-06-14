968f84372edbe24d9049b153cacd60dbd5101592
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-06-14 21:49:54 +0100

    Add the submit name and score event listener.

--- src/modules/EventLister.js
+++ src/modules/EventLister.js
@@ -1,16 +1,35 @@
-import DisplayOnBoard from './UserInterface.js'
+import DisplayOnBoard from './UserInterface.js';
+import Utility from './utility.js';
 
 export default class EventListenerClass {
     static windowLoad = () => {
-        window.addEventListener('load', async () => {
-            try {
-                const displayIndexScoreName = await DisplayOnBoard.displayScore()
-                return displayIndexScoreName
-            } catch (error) {
-                throw new Error(error);
-              }
-        })
-       
-        
+      window.addEventListener('load', async () => {
+        try {
+          const displayIndexScoreName = await DisplayOnBoard.displayScore();
+          return displayIndexScoreName;
+        } catch (error) {
+          throw new Error(error);
+        }
+      });
+    }
+
+    static refreshBTNListener = () => {
+      const submitForm = document.querySelector('form');
+      submitForm.addEventListener('submit', async (e) => {
+        try {
+          e.preventDefault();
+          const [name, score] = Array.from(submitForm.elements);
+          const createNewScore = await Utility.createNewScoreAPI({
+            name: name.value,
+            score: score.value,
+          });
+
+          name.value = '';
+          score.value = '';
+          return createNewScore;
+        } catch (error) {
+          throw new Error(error);
+        }
+      });
     }
 }
