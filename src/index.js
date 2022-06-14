b85f398278badfccd1e24079bf3df4c111cbdcdc
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-06-14 18:39:58 +0100

    Changed the file name properly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import UI from './modules/UI.js';
-import GetData from './modules/getdata.js';
+import GetData from './modules/AccessKey.js';
 
 UI.interface();
 GetData.getData();
