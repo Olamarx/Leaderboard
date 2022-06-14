9f2f2de587a0a9173e27844cf5e80265071af079
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-06-14 18:38:26 +0100

    Deleted the hardcoded display and got the access to the endpoint.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 import './style.css';
 import UI from './modules/UI.js';
-import GetData from './modules/getdata'
+import GetData from './modules/getdata.js';
 
 UI.interface();
-GetData.getData()
+GetData.getData();
+
+// getData();
--- src/modules/getdata.js
+++ src/modules/getdata.js
@@ -1,15 +1,11 @@
 export default class GetData {
-    static getData = async () => {
-        const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/WHYyghGGuwp0kRjzRRn5/scores', {
-            method: 'POST',
-            headers: {'content-Type': 'application/json'},
-            body: JSON.stringify({name: 'Owoeye Olaoluwa\'s Leaderboard'}),
-        })
-        try {
-            const reply = response.json()
-            console.log(reply)
-        } catch (error) {
-            throw new Error(error)
-        }
+ static getData = async () => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    headers: { 'content-Type': 'application/json' },
+    body: JSON.stringify({ name: 'Owoeye Olaoluwa\'s Leaderboard' }),
+  })
+    .then((response) => response.text())
+    .then((res) => console.log(res.split(' ').filter((item) => item.length === 20)))
+};
     }
-}
