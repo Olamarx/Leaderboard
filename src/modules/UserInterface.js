48cdbed40fda28f8506a2a34466adc70ac74aded
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-06-14 21:04:46 +0100

    Add the DisplayOnBoard for the UI

---
+++ src/modules/UserInterface.js
@@ -0,0 +1,35 @@
+import Utility from './utility.js'
+import LeaderboardClass from './Leaderboard.js'
+
+export default class DisplayOnBoard {
+
+    static displayScore = async () => {
+        try {
+            const sortedScore = DisplayOnBoard.sortScore();
+            sortedScore.forEach((score, index) => {
+                DisplayOnBoard.htmlforDOM(score, index + 1)
+            })
+        }  catch (error) {
+            throw new Error(error);
+          }
+    }
+
+    static htmlforDOM = ({user, score}, index) => {
+        const UL = document.querySelector('ul')
+        const LI = document.createElement('li')
+        LI.classList.add('score-li')
+        LI.innerHTML = `<span> ${index} </span> <span> ${user} </span> <span> ${score} </span>`
+        UL.appendChild(LI)
+    } 
+
+
+    static sortScore = async () => {
+        try {
+            const gameUrl = Utility.URLandAccessKey()
+            const scores = await LeaderboardClass.getScore({url: gameUrl})
+            return await scores.sort((a,b) => b.score - a.score);
+        }  catch (error) {
+            throw new Error(error);
+          }
+    }
+}
